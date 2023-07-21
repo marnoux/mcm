c6def69ae1a42484d5578c4a428cb87987e1f20b
Marnoux <marnouxmanser@gmail.com>
2023-07-21 12:48:11 +0200

    Move images to public folder

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,37 @@
 const About = () => {
-	return (
-		<div className='flex justify-evenly gap-12 p-12 h-full bg-[#f3f1f0]'>
-			<div className='flex'>
-				<img
-					className='w-96 h-96 rounded-full'
-					src='/src/assets/img/bigLogo.webp'
-					alt='bigLogo'
-					width={1200}
-					height={1200}
-				/>
-			</div>
-			<div className='flex flex-col gap-5 w-64'>
-				<h1 className='text-2xl font-bold'>Our promise</h1>
-				<p>
-					Since 1997, mcm Promotions strove to deliver the best possible solution to your company`s
-					branding needs. This is achieved through honest, reliable service and expertise by a team
-					of innovative branding experts.
-				</p>
-				<p>
-					We provide high quality vehicle grade decals, vehicle branding solutions and guaranteed to
-					exceed your expectations.
-				</p>
-				<p>
-					All orders is carefully handled from start to finish by a single representative. We want
-					our customers to work with the same friendly person each time they order from us.
-				</p>
-				<p>
-					We promise the highest quality products, market related costs, the fastest turnaround time
-					and tailor made products to suit your every need. Delivered to your door.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-evenly gap-12 p-12 h-full bg-[#f3f1f0]">
+      <div className="flex">
+        <img
+          className="w-96 h-96 rounded-full"
+          src="/img/bigLogo.webp"
+          alt="bigLogo"
+          width={1200}
+          height={1200}
+        />
+      </div>
+      <div className="flex flex-col gap-5 w-64">
+        <h1 className="text-2xl font-bold">Our promise</h1>
+        <p>
+          Since 1997, mcm Promotions strove to deliver the best possible solution to your company`s
+          branding needs. This is achieved through honest, reliable service and expertise by a team
+          of innovative branding experts.
+        </p>
+        <p>
+          We provide high quality vehicle grade decals, vehicle branding solutions and guaranteed to
+          exceed your expectations.
+        </p>
+        <p>
+          All orders is carefully handled from start to finish by a single representative. We want
+          our customers to work with the same friendly person each time they order from us.
+        </p>
+        <p>
+          We promise the highest quality products, market related costs, the fastest turnaround time
+          and tailor made products to suit your every need. Delivered to your door.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,47 @@
 import {
-	PhoneIcon,
-	EnvelopeIcon,
-	MagnifyingGlassIcon,
-	ShoppingBagIcon,
-	ChevronDownIcon,
+  PhoneIcon,
+  EnvelopeIcon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  ChevronDownIcon,
 } from '@heroicons/react/24/outline';
 
 const Header = () => {
-	return (
-		<div className='flex justify-between bg-[#e1f0fb] w-auto h-auto p-4 px-8 items-center'>
-			<a href='#'>
-				<img src='/src/assets/img/logo.png' alt='logo' width={200} height={200} />
-			</a>
-			<div>
-				<ul className='flex align-center gap-12'>
-					<li className='flex align-center gap-2 text-align-center'>
-						Store
-						<ChevronDownIcon className='align-center h-auto w-3' />
-					</li>
-					<li>
-						<a href='/#about'>About Us</a>
-					</li>
-					<li>
-						<a href='/#contact'>Contact Us</a>
-					</li>
-				</ul>
-			</div>
-			<div className='flex gap-8 divide-x divide-gray-500'>
-				<div className='flex gap-4'>
-					<a href='tel:+27823363494' target='_blank'>
-						<PhoneIcon className='h-7 w-7' />
-					</a>
-					<a href='mailto:arno@mcmpromotions.co.za' target='_blank'>
-						<EnvelopeIcon className='h-7 w-7' />
-					</a>
-				</div>
-				<div className='flex gap-4 pl-6'>
-					<MagnifyingGlassIcon className='h-7 w-7' />
-					<ShoppingBagIcon className='h-7 w-7' />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-between bg-[#e1f0fb] w-auto h-auto p-4 px-8 items-center">
+      <a href="#">
+        <img src="/img/logo.png" alt="logo" width={200} height={200} />
+      </a>
+      <div>
+        <ul className="flex align-center gap-12">
+          <li className="flex align-center gap-2 text-align-center">
+            Store
+            <ChevronDownIcon className="align-center h-auto w-3" />
+          </li>
+          <li>
+            <a href="/#about">About Us</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex gap-8 divide-x divide-gray-500">
+        <div className="flex gap-4">
+          <a href="tel:+27823363494" target="_blank">
+            <PhoneIcon className="h-7 w-7" />
+          </a>
+          <a href="mailto:arno@mcmpromotions.co.za" target="_blank">
+            <EnvelopeIcon className="h-7 w-7" />
+          </a>
+        </div>
+        <div className="flex gap-4 pl-6">
+          <MagnifyingGlassIcon className="h-7 w-7" />
+          <ShoppingBagIcon className="h-7 w-7" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
