850a5274266c6d7e677c73852083205784681913
marnoux <marnouxmanser@gmail.com>
2023-07-24 23:27:13 +0200

    Some finishing touches

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Hero = () => {
   return (
     <div className="flex flex-col justify-start align-center text-center gap-20  bg-gradient-to-r from-sky-500 to-indigo-500 opacity-80 pb-14">
@@ -16,11 +18,11 @@ const Hero = () => {
         </p>
       </div>
       <div>
-        <a href="https://decalsonline.co.za" className="animateHover" target="_blank">
+        <Link to="store" className="animateHover">
           <button className="rounded w-full py-2 px-4 animateHover max-w-xs text-gray-500  border border-gray-500 hover:border-sky-500 hover:text-sky-500">
             Shop now
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,17 @@
 const Footer = () => {
   return (
-    <div className="bg-[#9aaab8] flex justify-center">
-      <p className="m-3">© {new Date().getFullYear()} MCM Promotions</p>
+    <div className="bg-[#9aaab8] flex flex-col items-center justify-center">
+      <p className="mt-3">© {new Date().getFullYear()} MCM Promotions</p>
+      <p className="mb-3">
+        Created by{' '}
+        <a
+          className="hover:underline text-sky-50 transition-colors duration-200 hover:text-teal-400"
+          href="https://www.marnoux.dev"
+          target="_blank"
+        >
+          Marnoux
+        </a>
+      </p>
     </div>
   );
 };
