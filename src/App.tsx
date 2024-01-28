79bb442f856874f7b0e9c5cc51dd952332322c31
Marnoux Manser <marnouxmanser@gmail.com>
2024-01-28 17:14:11 +0100

    Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Layout/Footer';
-import Hero from './components/Hero';
-import Header from './components/Layout/Header';
-import { Route, Routes, useLocation } from 'react-router';
-import Store from './components/Store';
-import GoToTop from './components/GoToTop';
-import ErrorPage from './error-page';
-
 const App = () => {
-  const location = useLocation();
+  window.location.href = 'https://sswsigns.co.za/';
 
-  return (
-    <div className="flex flex-col h-[100vh] min-h-[100vh]">
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <main className="flex-1">
-              <section id="hero">
-                <Hero />
-              </section>
-
-              <section id="about">
-                <About />
-              </section>
-
-              <section id="contact">
-                <Contact />
-              </section>
-            </main>
-          }
-        />
-        <Route path="/store" element={<Store />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      {location.pathname === '/' && <GoToTop />}
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Analytics />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    <App />
+    {/* <Analytics /> */}
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 );
